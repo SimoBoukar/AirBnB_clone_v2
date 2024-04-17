f713cec4d13714e1f106d84616be58059bad0638
Mohammed <6bouass9@gmail.com>
2024-04-18 00:59:10 +0100

    update place.py

--- models/place.py
+++ models/place.py
@@ -5,6 +5,19 @@ from sqlalchemy.orm import relationship
 import models
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 
+classes = {"BaseModel": BaseModel, "User": User,
+            "Review": Review, "City": City,
+            "State": State, "Place": Place,
+            "Amenity": Amenity}
+
+if models.storage_engine == "db":
+    place_amenity = Table('place_amenity', Base.metadata,
+                            Column('place_id', String(60),
+                                ForeignKey('places.id'),
+                                primary_key=True, nullable=False),
+                            Column('amenity_id', String(60),
+                                ForeignKey('amenities.id'),
+                                primary_key=True, nullable=False))
 class Place(BaseModel, Base):
     """ A place to stay """
     if models.storage_engine == "db":
@@ -37,3 +50,30 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+        @property
+        def reviews(self):
+            """getter function for reviews"""
+            result = []
+            temp = classes['Review']
+            for i in models.storage.all(temp).values():
+            if i.place_id == self.id:
+                result.append(i)
+            return result
+
+        @property
+        def amenities(self):
+            """getter function for amenity"""
+            result = []
+            temp = classes['Amenity']
+            for a in models.storage.all(temp).values():
+            if a in self.amenity_ids:
+                result.append(a)
+            return result
+
+        @amenities.setter
+        def amenities(self, obj):
+            """setter for amenities class"""
+            temp = classes['Amenity']
+            if (isinstance(obj, models.storage.all(temp))):
+                self.amenity_ids.append(obj.id)
