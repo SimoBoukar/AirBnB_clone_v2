e35eeaebee19b0e0bf35f8b779d11bde4f105462
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-04-18 01:36:00 +0100

    updates 2.0

--- models/place.py
+++ models/place.py
@@ -6,18 +6,18 @@ import models
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 
 classes = {"BaseModel": BaseModel, "User": User,
-            "Review": Review, "City": City,
-            "State": State, "Place": Place,
-            "Amenity": Amenity}
+           "Review": Review, "City": City,
+           "State": State, "Place": Place,
+           "Amenity": Amenity}
 
 if models.storage_engine == "db":
     place_amenity = Table('place_amenity', Base.metadata,
-                            Column('place_id', String(60),
-                                ForeignKey('places.id'),
-                                primary_key=True, nullable=False),
-                            Column('amenity_id', String(60),
-                                ForeignKey('amenities.id'),
-                                primary_key=True, nullable=False))
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -36,9 +36,9 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place")
         amenities = relationship("Amenity",
-                            secondary="place_amenity",
-                            backref="place_amenities",
-                            viewonly=False)
+                                 secondary="place_amenity",
+                                 backref="place_amenities",
+                                 viewonly=False)
 
     else:
         city_id = ""
