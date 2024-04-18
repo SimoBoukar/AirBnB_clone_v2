74c72b7d8f1c864a3ccdb6ebe86b5e7aeddd30e7
Mohammed <6bouass9@gmail.com>
2024-04-18 03:49:11 +0100

    fix errors in tasks 6, 7, 8 + pycodestyle v2

--- models/user.py
+++ models/user.py
@@ -8,10 +8,10 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = Column("email", String(128), nullable=False)
+    password = Column("password", String(128), nullable=False)
+    first_name = Column("first_name", String(128), nullable=True, default="NULL")
+    last_name = Column("last_name", String(128), nullable=True, default="NULL")
     places = relationship("Place", cascade="delete",
                           backref="user")
     reviews = relationship("Review", cascade="delete",
