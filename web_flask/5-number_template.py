8ade4eed484ce473a1a27fd042854f936679602a
Mohammed <6bouass9@gmail.com>
2024-05-19 12:47:53 +0100

    push task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """Print Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Print HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """Print C followed by the value of <text>
+    Replaces any underscores in <text> with slashes."""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python(text="is cool"):
+    """Print Python followed by the value of <text>
+    Replaces any underscores in <text> with slashes."""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """Print 'n is a number' only if <n> is an integer."""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """Print an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
